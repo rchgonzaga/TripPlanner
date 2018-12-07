b306147d53fe9445abb03a0e86f0bd78da623381
rchgonzaga@gmail.com <rchgonzaga@gmail.com>
2018-12-07 03:02:39 -0200

    Added images and homescreen actions

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground, Image, TouchableWithoutFeedback } from 'react-native';
 
 class HomeScreen extends Component {
+
+  state = {
+    counter: 0
+  }
+
+  handleCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
   render() {
     return (
-      <View>
-        <Text>
-          Aowwww
-        </Text>
-      </View>
+      <ImageBackground
+        source={require('../../assets/background.png')}
+        imageStyle={{ resizeMode: 'stretch' }}
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          alignItems: 'stretch',
+          justifyContent: 'space-between'
+        }}
+      >
+        <View style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+          <Image source={require('../../assets/logo.png')} />
+        </View>
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          alignItems: 'flex-end',
+          justifyContent: 'center',
+          paddingBottom: 32
+        }}>
+          <Image source={require('../../assets/point.png')} />
+        </View>
+
+        <TouchableWithoutFeedback onPress={this.handleCounter}>
+          <View style={{ backgroundColor: 'green', paddingBottom: 16, }}>
+            <Text style={{ textAlign: 'center' }}>Aowwwws {this.state.counter}</Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </ImageBackground>
     )
   }
 }
